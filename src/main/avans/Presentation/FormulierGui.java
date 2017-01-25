e2d0221e7b62c9160da1bb91d7a8e4cb9e21762e
rowanverschuren <rowanverschuren@hotmail.com>
2017-01-25 21:25:44 +0100

    pompom

--- src/main/avans/Presentation/FormulierGui.java
+++ src/main/avans/Presentation/FormulierGui.java
@@ -3,7 +3,7 @@
  * To change this template , choose Tools | Templates
  * and open the template in the editor.
  */
-package Presentation;
+package avans.Presentation;
 
 import datastorage.connection;
 import datastorage.FormulierDAO;
--- src/main/avans/datastorage/FormulierDAO.java
+++ src/main/avans/datastorage/FormulierDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package datastorage;
+package avans.datastorage;
 
 
 import domain.Formulier;
